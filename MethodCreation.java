1cd37a0532e6710b6bce82dc284c9e00a626b308
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-23 08:55:03 +0530

    Refactor objectCreation and add filteringByPrice method
    Updated objectCreation method to return ProductDetails and added filteringByPrice method to filter products by price.

--- MethodCreation.java
+++ MethodCreation.java
@@ -1,16 +1,54 @@
 package BasicPractice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodCreation {
 	
-	public static void objectCreation(int id, String product, String desc, int price ) {
+	public static ProductDetails objectCreation(int id, String product, String desc, int price ) {
 		ProductDetails details = new ProductDetails(id, product, desc, price);
 		System.out.println(details);
+		return details;
 		
 	}
 	
+	public static int calculate(int a, int b) {
+		return a*b;
+	}
+	
+	
+	public static List<ProductDetails> filteringByPrice(int price, List<ProductDetails> details) {
+		
+		List<ProductDetails> result = new ArrayList<>();
+		
+		for(ProductDetails product: details) {
+			
+			if(product.getPrice() <= price) {
+				result.add(product);
+			}
+		}
+		
+		return result;	
+	}
+	
 	public static void main(String[] args) {
-		objectCreation(1, "asdsadad", "fafasdas", 1111);
-		objectCreation(2, "sredasda", "wrerferwererwe", 23343243);
+//		objectCreation(1, "asdsadad", "fafasdas", 1111);
+		ProductDetails result =  objectCreation(1, "sredasda", "wrerferwererwe", 8000);
+		ProductDetails result1 =  objectCreation(2, "mobile", "wrerferwererwe", 10000);
+		ProductDetails result2 =  objectCreation(3, "mobile", "wrerferwererwe", 50000);
+		
+		List<ProductDetails> input = new ArrayList<>();
+		input.add(result);
+		input.add(result1);
+		input.add(result2);
+		
+		
+		if(result.id == 2) {
+			System.out.println("object created successfully");
+		}
+		
+		List<ProductDetails> filterResult = filteringByPrice(10000, input);
+		System.out.println(filterResult);
 		
 	}
 
