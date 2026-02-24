1334384323eb040d69a58296bc4154d5f26f2961
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-02-24 09:16:04 +0530

    Add abstract class sampleAbstract with methods

---
+++ sampleAbstract.java
@@ -0,0 +1,11 @@
+package BasicPractice;
+
+abstract class sampleAbstract {
+	
+	public void createObject() {}
+	
+	public void printFunction() {
+		System.out.println("The abstract class is working fine");
+	}
+
+}
