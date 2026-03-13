b19283c24a98fb8f66cb34c8928d3987c3b32cb1
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-03-13 08:47:08 +0530

    Add BooksData class for book information management

---
+++ BooksData.java
@@ -0,0 +1,39 @@
+package DataBaseConnection;
+
+public class BooksData {
+	
+	int id;
+	String book_name;
+	String author;
+	int price;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getBook_name() {
+		return book_name;
+	}
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "BooksData [id=" + id + ", book_name=" + book_name + ", author=" + author + ", price=" + price + "]";
+	}
+}
