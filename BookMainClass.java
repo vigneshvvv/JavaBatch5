9fe7fcc5ba9c3529dc00486279e9ad53c584634c
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-03-13 08:48:04 +0530

    Create BookMainClass to retrieve book data

---
+++ BookMainClass.java
@@ -0,0 +1,10 @@
+package DataBaseConnection;
+
+public class BookMainClass {
+	
+	public static void main(String[] args) {
+		BookDataBase base = new BookDataBase();
+		base.getBookDataFromDB();
+	}
+
+}
